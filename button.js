b1ecfd6d3114d4f66f317c3f9a9c4244d21a4fd1
andrew <andrew@selwood.ca>
2016-09-14 13:54:07 -0400

    First Go

---
+++ button.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+// import css
+import './Button.css';
+
+// import component
+import Button from './Button.js';
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+            <button color='#444'/>
+      </div>
+
+    )
+    }
+  }
+}
